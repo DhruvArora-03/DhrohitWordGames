c35d5787f20a00e05a753659c81cc7fd959dcc75
Rohit Anantha <anantha.rohit.11@gmail.com>
2022-03-31 17:36:13 -0500

    use for testing

--- letterBoxedClone.js
+++ letterBoxedClone.js
@@ -10,6 +10,13 @@ function initGame() {
 
 }
 
+
+// returns an array of arrays of 3 letters each
+// currently for testing purposes
+function get_words() {
+    return [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l']]
+}
+
 function handleKeyPress(e) {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
         console.log("letter pressed");
