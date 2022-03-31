0886d4b921c65578e3b529556101ebe3c51ccaa2
Rohit Anantha <anantha.rohit.11@gmail.com>
2022-03-31 18:24:05 -0500

    added some minor things, testing and copied from wordle

--- letterBoxedClone.js
+++ letterBoxedClone.js
@@ -9,9 +9,50 @@ const VOWELS = Array.from("AEIOU");
 
 const ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
+// setup canvas and constants
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = Math.floor(canvas.getBoundingClientRect().width);
+const CANVAS_HEIGHT = Math.floor(canvas.getBoundingClientRect().height);
+
+const TILE_WIDTH = CANVAS_WIDTH / 5;
+const TILE_HEIGHT = CANVAS_HEIGHT / 6;
+console.log(CANVAS_WIDTH);
+console.log(CANVAS_HEIGHT);
+
+const BACKGROUND_COLOR = "#ECECEC";
+const GREEN = "#008000";
+const YELLOW = "#A6A600";
+const GRAY = "#373737";
+
 
 function initGame() {
 
+}
+
+resetGame()
+
+function resetGame() {
+    // clear anything from current instance of game
+    ctx.fillStyle = BACKGROUND_COLOR;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    winLossText.textContent = "";
+
+    drawLetterSquares();
+    console.log("board drawn");
+
+    //wordToGuess = generateWordToGuess();
+    console.log(`Word to guess is ${wordToGuess}`);
+
+    // keep track of current guess and number of guesses
+    gameIsGoing = true;
+    lettersTyped = 0;
+    numGuesses = 0;
+    guess = "";
+    correct = true; // if current guess is correct
+
+    // take away focus from button
+    resetButton.blur();
 }
 
 
@@ -19,6 +60,15 @@ function initGame() {
 // currently for testing purposes
 function get_words() {
     return [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l']]
+}
+
+function drawLetterSquares() {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    // draw a large square
+    ctx.rect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.stroke();
 }
 
 function handleKeyPress(e) {
